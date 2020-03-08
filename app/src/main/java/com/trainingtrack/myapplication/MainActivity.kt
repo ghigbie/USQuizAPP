02cd2201bfc328ddc61c9ac0faf05a603c1d7200
ghigbie <gehigbie@gmail.com>
2020-03-07 17:40:18 -0700

    Add new toast

--- app/src/main/java/com/trainingtrack/myapplication/MainActivity.kt
+++ app/src/main/java/com/trainingtrack/myapplication/MainActivity.kt
@@ -2,6 +2,11 @@ package com.trainingtrack.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        false_button.setOnClickListener{
+            Toast.makeText(this, "False", Toast.LENGTH_SHORT).show()
+        }
+
+        true_button.setOnClickListener {
+            Toast.makeText(this, "True", Toast.LENGTH_SHORT).show()
+        }
+
+        question
+
     }
+
+
+
+
 }
